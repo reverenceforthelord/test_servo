a364acaa0ed7373a68c74f9477172454e842f856
Shinsuke Tajiri <91228275+reverenceforthelord@users.noreply.github.com>
2022-03-10 13:05:06 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,37 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    start = 1
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    start = 0
+})
 radio.onReceivedValue(function (name, value) {
-    if (name == "x") {
-        acc_x = value
+    if (name == "x" && start == 1) {
+        basic.showString("x")
+        angle = right_angle + pins.map(
+        value,
+        -1024,
+        1023,
+        0 - angle_range,
+        angle_range
+        )
+        pins.servoWritePin(AnalogPin.P8, angle)
     }
 })
-let acc_x = 0
+let angle = 0
+let angle_range = 0
+let right_angle = 0
+let start = 0
 radio.setGroup(1)
-let right_angle = 95
-let angle_range = 30
-let angle = right_angle
-pins.servoWritePin(AnalogPin.P2, right_angle)
+start = 0
+right_angle = 95
+angle_range = 30
+angle = right_angle
+pins.servoWritePin(AnalogPin.P8, angle)
 basic.forever(function () {
-    angle = right_angle + pins.map(
-    acc_x,
-    -1024,
-    1023,
-    0 - angle_range,
-    angle_range
-    )
-    pins.servoWritePin(AnalogPin.P2, angle)
+	
 })
